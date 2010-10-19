c4e9794d37f8429a9f65f3f22d599b16db6b673a
Sam Saffron <sam@stackoverflow.com>
2010-10-18 13:47:48 -1100

    added a couple of dbs

--- App/StackExchange.DataExplorer.DB/Scripts/Post-Deployment/Script.PostDeployment.sql
+++ App/StackExchange.DataExplorer.DB/Scripts/Post-Deployment/Script.PostDeployment.sql
@@ -145,3 +145,117 @@ color:#E7E7E7;
 text-decoration:none;}',
 'https://odata.sqlazurelabs.com/OData.svc/v0.1/rp1uiewita/Meta'
 	)
+
+GO
+
+if not exists(select * from Sites where Sites.Name = 'MetaSuperUser')
+  insert into Sites(
+	Id, 
+	TinyName,Name,LongName,
+	Url,ImageUrl,IconUrl,
+	DatabaseName,
+	Tagline,
+	TagCss,
+	ODataEndpoint) values 
+	(5, 
+	'msu', 'MetaSuperUser', 'Meta Super User',
+	'http://meta.superuser.com', 'http://sstatic.net/superusermeta/img/logo.png','http://sstatic.net/superusermeta/img/favicon.ico',
+	'Meta',
+	'Q&A about Super User',
+	'.post-tag  {
+background-color:#E7E7E7;
+border-bottom:1px solid #626262;
+border-right:1px solid #979797;
+color:#6F6F6F;
+font-size:90%;
+line-height:2.4;
+margin:2px 2px 2px 0;
+padding:3px 4px;
+text-decoration:none;
+white-space:nowrap;
+}
+.post-tag:visited {
+color:#6F6F6F;
+}
+.post-tag:hover {
+background-color:#626262;
+border-bottom:1px solid #565656;
+border-right:1px solid #565656;
+color:#E7E7E7;
+text-decoration:none;}',
+''
+	)
+	
+GO
+
+if not exists(select * from Sites where Sites.Name = 'MetaServerFault')
+  insert into Sites(
+	Id, 
+	TinyName,Name,LongName,
+	Url,ImageUrl,IconUrl,
+	DatabaseName,
+	Tagline,
+	TagCss,
+	ODataEndpoint) values 
+	(6, 
+	'msf', 'MetaServerFault', 'Meta Server Fault',
+	'http://meta.serverfault.com', 'http://sstatic.net/serverfaultmeta/img//logo.png','http://sstatic.net/serverfaultmeta/img/favicon.ico',
+	'Meta',
+	'Q&A about Server Fault',
+	'.post-tag  {
+background-color:#E7E7E7;
+border-bottom:1px solid #626262;
+border-right:1px solid #979797;
+color:#6F6F6F;
+font-size:90%;
+line-height:2.4;
+margin:2px 2px 2px 0;
+padding:3px 4px;
+text-decoration:none;
+white-space:nowrap;
+}
+.post-tag:visited {
+color:#6F6F6F;
+}
+.post-tag:hover {
+background-color:#626262;
+border-bottom:1px solid #565656;
+border-right:1px solid #565656;
+color:#E7E7E7;
+text-decoration:none;}',
+''
+	)
+
+	if not exists(select * from Sites where Sites.Name = 'WebApps')
+   insert into Sites(
+	Id, 
+	TinyName,Name,LongName,
+	Url,ImageUrl,IconUrl,
+	DatabaseName,
+	Tagline,
+	TagCss,
+	ODataEndpoint) values
+	(7, 
+	'webapps', 'WebApps', 'Web Apps',
+	'http://webapps.stackexchange.com', 'http://sstatic.net/webapps/img/logo.png', 'http://sstatic.net/webapps/img/favicon.ico',
+	'Web Apps',
+	'Q&A for power users of web applications', 
+	'.post-tag{
+background-color:#E0EAF1;
+border-bottom:1px solid #3E6D8E;
+border-right:1px solid #7F9FB6;
+color:#3E6D8E;
+font-size:90%;
+line-height:2.4;
+margin:2px 2px 2px 0;
+padding:3px 4px;
+text-decoration:none;
+white-space:nowrap;
+}
+.post-tag:hover {
+background-color:#3E6D8E;
+border-bottom:1px solid #37607D;
+border-right:1px solid #37607D;
+color:#E0EAF1;
+text-decoration:none;}',
+'')
