a049e637bb6bb23062290fe3c5aa37fd3d6ce4d1
Sam Saffron <sam@stackoverflow.com>
2010-12-07 16:21:27 -1100

    missing table and index

---
+++ Migrations/010 - CachedResults.sql
@@ -0,0 +1,16 @@
+﻿if OBJECT_ID('CachedResults') is null
+begin
+
+CREATE TABLE [dbo].[CachedResults](
+	[QueryHash] [uniqueidentifier] NULL,
+	[SiteId] [int] NULL,
+	[Results] [nvarchar](max) NULL,
+	[CreationDate] [datetime] NULL,
+	[Id] [int] IDENTITY(1,1) NOT NULL
+)
+
+create clustered index idx_results on CachedResults([QueryHash], SiteId)
+
+end
+
+
---
+++ Migrations/018 - CachedResults Indexes.sql
@@ -0,0 +1,9 @@
+﻿if dbo.fnIndexExists('CachedResults', 'idxId') = 1
+begin
+	drop index CachedResults.idxId
+end
+go
+if dbo.fnIndexExists('CachedResults', 'idxIdPrimaryKey') = 0
+begin
+	create unique index idxIdUnique on CachedResults (Id)
+end
