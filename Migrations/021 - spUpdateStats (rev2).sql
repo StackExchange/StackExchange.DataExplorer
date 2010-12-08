a9acb9aee5b19ca64a624029b27fb2d709770159
Sam Saffron <sam@stackoverflow.com>
2010-12-08 10:31:27 -1100

    another fix

--- Migrations/021 - spUpdateStats (rev2).sql
+++ Migrations/021 - spUpdateStats (rev2).sql
@@ -27,7 +27,8 @@ BEGIN
 	exec('use [' + @database_name + '] update [' + @local + '].dbo.Sites set TotalQuestions = (select count(*) from Posts where PostTypeId = 1) where Id = ' + @id_string)
 	exec('use [' + @database_name + '] update [' + @local + '].dbo.Sites set TotalAnswers = (select count(*) from Posts where PostTypeId = 2) where Id = ' + @id_string)
 	exec('use [' + @database_name + '] update [' + @local + '].dbo.Sites set TotalUsers = (select count(*) from Users) where Id = ' + @id_string)		
-	exec('use [' + @database_name + '] update [' + @local + '].dbo.Sites set TotalComments = (select count(*) from PostComments) where Id = ' + @id_string)	
+	exec('use [' + @database_name + '] if object_id(''Comments'') is not null update [' + @local + '].dbo.Sites set TotalComments = (select count(*) from Comments) where Id = ' + @id_string)	
+	exec('use [' + @database_name + '] if object_id(''PostComments'') is not null update [' + @local + '].dbo.Sites set TotalComments = (select count(*) from PostComments) where Id = ' + @id_string)	
 	exec('use [' + @database_name + '] update [' + @local + '].dbo.Sites set TotalTags = (select count(*) from Tags) where Id = ' + @id_string)	
 	exec('use [' + @database_name + '] update [' + @local + '].dbo.Sites set LastPost = (select p.CreationDate from Posts p where p.Id = (select max(pp.Id) from Posts pp)) where Id = ' + @id_string)		
 	fetch next from sites_cursor INTO @id, @database_name
