1b24dd52e48424dd7d26382c28ab3804ed645455
Justin Pealing <Justin.Minnow@gmail.com>
2011-01-28 20:44:51 -1000

    Workaround for caching issue when downloading execution plans.

--- App/StackExchange.DataExplorer/Helpers/QueryPlanResult.cs
+++ App/StackExchange.DataExplorer/Helpers/QueryPlanResult.cs
@@ -12,6 +12,15 @@ namespace StackExchange.DataExplorer.Helpers
             results = QueryResults.FromJson(json);
         }
 
+        private string PanlXml
+        {
+            get
+            {
+                // TODO: This only returns the first plan
+                return this.results.ExecutionPlans.Count == 0 ? null : results.ExecutionPlans[0];
+            }
+        }
+
         public override void ExecuteResult(ControllerContext context)
         {
             HttpResponseBase response = context.HttpContext.Response;
@@ -22,8 +31,7 @@ namespace StackExchange.DataExplorer.Helpers
             response.ContentType = "text/xml";
             response.AddHeader("content-disposition", attachment);
             response.AddHeader("Pragma", "public");
-            // TODO: This only returns the first plan
-            response.Write(results.ExecutionPlans[0]);
+            response.Write(this.PanlXml);
             response.Flush();
             response.Close();
         }
