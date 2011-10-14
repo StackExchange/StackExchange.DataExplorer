aa2ee4d90e3959a84eaf01bb80c28ab7da9330d4
Tim Stone <tmslft@gmail.com>
2011-10-15 03:25:20 +0400

    Made sure the QueryResults are transformed to actual responses

--- App/StackExchange.DataExplorer/Controllers/QueryController.cs
+++ App/StackExchange.DataExplorer/Controllers/QueryController.cs
@@ -49,12 +49,13 @@ namespace StackExchange.DataExplorer.Controllers
                     crossSite == true,
                     excludeMetas == true
                 );
+                QueryResults results = null;
 
                 // Just in case we get into a situation where we want to create a
                 // new revision but not run anything in the process.
                 if (executeQuery)
                 {
-                    var results = ExecuteWithResults(parsedQuery, siteId.Value, textResults == true);
+                    results = ExecuteWithResults(parsedQuery, siteId.Value, textResults == true);
                 }
 
                 var query = Current.DB.Query<Query>(
@@ -119,6 +120,11 @@ namespace StackExchange.DataExplorer.Controllers
                     queryId = query.Id;
                 }
 
+                if (executeQuery)
+                {
+                    QueryRunner.LogQueryExecution(CurrentUser, siteId.Value, queryId);
+                }
+
                 if (!saveId.HasValue)
                 {
                     throw new ApplicationException("Unable to save revision");
@@ -126,7 +132,14 @@ namespace StackExchange.DataExplorer.Controllers
 
                 if (executeQuery)
                 {
-                    QueryRunner.LogQueryExecution(CurrentUser, siteId.Value, queryId);
+                    // Need to fix up the way we pass back results
+                    results.QueryId = saveId.Value;
+                    // Consider handling this XSS condition (?) in the ToJson() method instead, if possible
+                    response = Content(results.ToJson().Replace("/", "\\/"), "application/json");
+                }
+                else
+                {
+                    // Return a response appropriate for an executionless creation
                 }
             }
             catch (Exception ex)
@@ -173,6 +186,9 @@ namespace StackExchange.DataExplorer.Controllers
 
                 var results = ExecuteWithResults(parsedQuery, siteId, textResults == true);
                 QueryRunner.LogQueryExecution(CurrentUser, siteId, query.Id);
+
+                // Consider handling this XSS condition (?) in the ToJson() method instead, if possible
+                response = Content(results.ToJson().Replace("/", "\\/"), "application/json");
             }
             catch (Exception ex)
             {
