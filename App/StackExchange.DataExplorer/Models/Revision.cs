79a0fd90f603495305e0bba85c98f843f181a9a3
Tim Stone <tmslft@gmail.com>
2011-10-19 06:57:25 +0400

    Turned Revision into a complete POCO

--- App/StackExchange.DataExplorer/Models/Revision.cs
+++ App/StackExchange.DataExplorer/Models/Revision.cs
@@ -1,7 +1,16 @@
-﻿namespace StackExchange.DataExplorer.Models
+﻿using System;
+
+namespace StackExchange.DataExplorer.Models
 {
-    public partial class Revision
+    public class Revision
     {
+        public int Id { get; set; }
+        public int QueryId { get; set; }
+        public int RootId { get; set; }
+        public int OwnerId { get; set; }
+        public string OwnerIP { get; set; }
+        public bool IsFeature { get; set; }
+        public DateTime CreationDate { get; set; }
         public Query Query { get; set; }
         public Metadata Metadata { get; set; }
     }
