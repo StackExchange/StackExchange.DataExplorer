1d92b231428bf78eb278958fe3a6b57471a9efc4
Sam Saffron <sam@stackoverflow.com>
2012-01-09 16:42:49 -1100

    more historic corrections, data was really mucked up

--- Migrations/041 - Missing index on RevisionExecutions idxUniqueExecutions.sql
+++ Migrations/041 - Missing index on RevisionExecutions idxUniqueExecutions.sql
@@ -1,4 +1,18 @@
 if dbo.fnIndexExists('RevisionExecutions','idxUniqueExecutions') = 0
 begin
+	
+	declare @rowcount int = 1
+	while @rowcount > 0 
+	begin 
+		delete RevisionExecutions where Id in (
+			select max(Id) from RevisionExecutions 
+			group by UserId, SiteId, RevisionId
+			having COUNT(*) > 1
+		)
+		set @rowcount = @@ROWCOUNT
+	end
+	
 	create unique index idxUniqueExecutions on RevisionExecutions(UserId, SiteId, RevisionId)
 end
+
+
