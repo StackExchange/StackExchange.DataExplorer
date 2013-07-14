16efc87b1747a539b395f03be9ba6fe3bab71cf8
tms <tmslft@gmail.com>
2013-07-14 17:32:45 -0400

    Start separating out JavaScript components for clarity

---
+++ App/StackExchange.DataExplorer/Scripts/query.sidebar.js
@@ -0,0 +1,119 @@
+﻿DataExplorer.Sidebar = (function () {
+    var schema, history, options, panel, panels, subpanels, toggle, sidebarPreference;
+
+    function init(o) {
+        schema = $('#schema');
+        history = $('#history');
+        subpanels = schema.add(history);
+        options = o;
+
+        panel = $(o.panel);
+        panels = panel.add(o.toolbar);
+        toggle = panels.find('#schema-toggle').on('click', toggleSidebar);
+
+        if (DataExplorer.options.User.isAuthenticated) {
+            sidebarPreference = new DataExplorer.DeferredRequest({
+                'url': '/users/save-preference/:id/HideSchema'.format({
+                    'id': DataExplorer.options.User.id
+                })
+            });
+
+            if (DataExplorer.options.User.hideSchema) {
+                toggleSidebar(false, true);
+            }
+        }
+
+        initSchema();
+    }
+
+    function initSchema() {
+        schema.TextAreaResizer(resizeSchema, {
+            'offsetTop': schema.find('.heading').outerHeight(),
+            'resizeSelector': 'ul'
+        });
+        schema.addClass('cm-s-' + options.editorTheme);
+        schema.on('click', '.schema-table', function () {
+            $(this).next('dl').toggle();
+        });
+        schema.find('.expand').on('click', function () {
+            schema.find('dl').show();
+        });
+        schema.find('.collapse').on('click', function () {
+            schema.find('dl').hide();
+        });
+    }
+
+    function toggleSidebar(show, immediately) {
+        if (show !== !!show) {
+            show = toggle.hasClass('hidden');
+        }
+
+        if (!show) {
+            subpanels.hide();
+        }
+
+        if (immediately !== true) {
+            panels.animate({ 'width': show ? '70%' : '100%' }, 'fast', function () {
+                if (show) {
+                    subpanels.show();
+                }
+            });
+        } else {
+            panels.css('width', show ? '70%' : '100%');
+        }
+
+        toggle.text((show ? 'hide' : 'show') + ' sidebar').toggleClass('hidden', !show);
+
+        if (sidebarPreference) {
+            sidebarPreference.request({ value: !show });
+        }
+    }
+
+    function resizeSchema() {
+        var available = panel.outerHeight(),
+            remaining = available - schema.outerHeight(),
+            list = history.children('ul'),
+            offset = history.outerHeight() - list.height();
+
+        list.height(remaining - offset);
+    }
+
+    function updateHistory(response) {
+        if (response.created) {
+            var title = response.created.replace(/\.\d+Z/, 'Z'),
+                href = "/" + response.siteName + "/revision/" + response.querySetId + "/" + response.revisionId + "/" + response.slug,
+                classes = "selected";
+
+            if (response.parentId) {
+                history.find('#revision-' + response.parentId).addClass('parent');
+            }
+
+            history.find('.empty').remove();
+            history.find('.selected').removeClass('selected');
+            history.children('ul').prepend(
+                '<li id="revision-' + response.revisionId + '" class="' + classes + '">' +
+                    '<a href="' + href + '">' +
+                        '<span class="revision-info">' + response.revisionId + '</span>' +
+                    '</a>' +
+                    '<span class="relativetime" title="' + title + '"></span>' +
+                    '<div style="clear:both"></div>' +
+                '</li>'
+            );
+            history.find('li:last').addClass('last');
+
+            if (window.history && window.history.pushState && document.URL.indexOf("query/edit") == -1) {
+                window.history.pushState(null, "", "/" + response.siteName + "/query/edit/" + response.querySetId);
+            }
+        }
+
+        history.find('.relativetime').each(function () {
+            this[_textContent] = Date.parseTimestamp(this.title).toRelativeTimeMini();
+        });
+    }
+
+    return {
+        init: init,
+        toggle: toggleSidebar,
+        updateHistory: updateHistory
+    };
+})();
