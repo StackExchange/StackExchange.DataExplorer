978e33fac3a932c852734b19fb9713c900248ab1
Tim Stone <tmslft@gmail.com>
2013-12-22 20:26:01 -0500

    Add Web Optimization dependency

---
+++ App/StackExchange.DataExplorer/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace StackExchange.DataExplorer.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+        }
+    }
+}
