45f35cb4e8d74a944e58a8a01962daffba72d511
Nick Craver <craver@stackoverflow.com>
2015-04-22 06:58:46 -0400

    Don't assume enterprise edition SQL

--- Migrations/054 - Add Users.ADLogin.sql
+++ Migrations/054 - Add Users.ADLogin.sql
@@ -5,6 +5,5 @@ End
 
 If dbo.fnIndexExists('Users','Idx_Users_ADLogin') = 0
 Begin
-  Create Nonclustered Index Users_ADLogin ON Users (ADLogin)
-  With (Online = On, Sort_in_TempDB = On, Drop_Existing = Off)
+  Create Nonclustered Index Users_ADLogin ON Users (ADLogin);
 End
